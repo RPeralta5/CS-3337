5401c8bfe7e73a3d0d1798dd9f6ed9330ed9f307
RPeralta5 <34075172+RPeralta5@users.noreply.github.com>
2018-11-30 23:35:34 -0800

    Updated version that has reference to highlighted image

--- ObjectRecognition.java
+++ ObjectRecognition.java
@@ -16,20 +16,26 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 /**
- * 
  * @author Ryan Peralta
- *
+ * Automatic License Plate Reader: Module 1
+ * Object Recognition
+ * Base image is used as an input, and a file object that references the cropped image is returned.
  */
 public class ObjectRecognition implements objectRecognitionModule {
+	private File highlight = null;
+	
 	public static void main(String[] args){
-		String path = "C:/Users/rpera/Documents/Cal State LA/2018-2019/Fall Semester 2018/CS 3337/Sample_Images/image_5.jpg";
+		String path = "C:/Users/rpera/Documents/Cal State LA/2018-2019/Fall Semester 2018/CS 3337/Sample_Images/image_1.jpg";
 		ObjectRecognition or = new ObjectRecognition();
 		File image = new File(path);
 		File cropped = or.findLicensePlateInImage(image);
 		try{
+			File high = or.getHighlight();
 			String x = cropped.getAbsolutePath();
+			String y = high.getAbsolutePath();
 			System.out.println("Input Image: " + path);
 			System.out.println("Cropped Image: " + x);
+			System.out.println("Highlighted Image: " + y);
 		}
 		catch(NullPointerException e){
 			System.out.println("Null Pointer Exception");
@@ -46,11 +52,7 @@ public class ObjectRecognition implements objectRecognitionModule {
 		Mat imageMat = Imgcodecs.imread(path);
 		Mat imageHSV = new Mat(imageMat.size(), CvType.CV_8UC4);
 	    Mat imageBlurr = new Mat(imageMat.size(),CvType.CV_8UC4);
-	    Mat imageA = new Mat(imageMat.size(), CvType.CV_32F);
-	    
-	    //EVERYTHING BEFORE THIS LINE IS CHECKED. 
-	    //TELL MARK THAT WE CAN USE MY DEMO CLASS FOR THE GUI, WE WILL RETURN THE PICTURE WITH THE RED BOX AROUND THE LICENSE PLATE!!!!
-	    
+	    Mat imageA = new Mat(imageMat.size(), CvType.CV_32F);	    
 	    
 	    //Applying different filters to the original image and saving them as separate Mats
 	    Size s = new Size(5,5);
@@ -59,15 +61,12 @@ public class ObjectRecognition implements objectRecognitionModule {
 	    double c = 5;
 	    Imgproc.cvtColor(imageMat, imageHSV, Imgproc.COLOR_BGR2GRAY);
 	    Imgproc.GaussianBlur(imageHSV, imageBlurr, s, 0);
-	    //Imgproc.adaptiveThreshold(imageBlurr, imageA, 255,Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY,7, 5);
 	    Imgproc.adaptiveThreshold(imageBlurr, imageA, maxValue,Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY,blockSize, c);
-	    //Imgproc.adaptiveThreshold(src, dst, maxValue, adaptiveMethod, thresholdType, blockSize, C);
+	    
 	    //Resizing the image so that less obscure and not necessary lines will show
 	    double threshold1 = 300;
 	    double threshold2 = 600;
 	    Imgproc.Canny(imageA, imageA, threshold1, threshold2);
-	    //Imgproc.Canny(imageA, imageA, 300, 600);
-	    //Imgproc.Canny
 	    
 	    //Creating a list of contours for the image with the applied filters.
 	    List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
@@ -75,17 +74,25 @@ public class ObjectRecognition implements objectRecognitionModule {
 	    
 	    //Creating a file object that will be returned once the method has completed
 	    File plate = null;
+	    //Creating Mat that clones the orignial, for the new highlited image
+	    Mat h = imageMat.clone();
 	    
-	    //int outCounter = 1;
 	    for(int i = 0; i < contours.size(); i++){
 	    	Rect rect = Imgproc.boundingRect(contours.get(i));
 	    	double area = Imgproc.contourArea(contours.get(i));
 	    	
 	    	//Specifying for a minimum area of 100, so that small, insignificant, contours will be filtered out
 	    	if(area > 100){
-		    	if (rect.width >= rect.height *1.8 && rect.width <= rect.height *2.2){	//Standard size is 	12 by 6          {
+	    		//Standard size is 12 by 6
+		    	if (rect.width >= rect.height *1.8 && rect.width <= rect.height *2.2){
+		    		//Creating the highlighted image
+		    		Imgproc.rectangle(h, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height),new Scalar(0,0,255),5);
+		    		String pathH  = "C:/Users/rpera/Documents/Cal State LA/2018-2019/Fall Semester 2018/CS 3337/Cropped_Output/highlight.jpg";
+		    		Imgcodecs.imwrite(pathH,h);
+		    		highlight= new File(pathH);
+
+		    		//Cropping the license plate
 		    		Mat crop = new Mat(imageMat, rect);
-		    		//cropped.add(crop);
 		    		Imgproc.rectangle(imageMat, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height),new Scalar(0,0,255));
 		    		String path2  = "C:/Users/rpera/Documents/Cal State LA/2018-2019/Fall Semester 2018/CS 3337/Cropped_Output/cropped_plate.jpg";
 		    		Imgcodecs.imwrite(path2,crop);
@@ -99,5 +106,10 @@ public class ObjectRecognition implements objectRecognitionModule {
 	    System.out.println("No license plate found!");
 	    return plate;
 	}
+	
+	public File getHighlight(){
+		return highlight;	
+	}
+	
 } 
 
